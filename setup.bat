126e09a5f118f703df4c929f70ea8c6583eed3b2
BlackCat <184516521+MeoMunDep@users.noreply.github.com>
2024-12-25 23:25:36 +0700

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,5 +1,5 @@
 @echo off
-title name Bot
+title Star Power Bot
 color 0A
 
 cd ..
@@ -14,7 +14,7 @@ if exist node_modules (
 :MENU
 cls
 echo =================================================================
-echo    name BOT SETUP AND RUN SCRIPT
+echo    Star Power BOT SETUP AND RUN SCRIPT
 echo =================================================================
 echo.
 echo Current directory: %CD%
@@ -97,7 +97,7 @@ if exist "..\node_modules" (
 ) else (
     echo Using node_modules from current directory
 )
-node bot
+cd star-power && node bot
 pause
 goto MENU
 
